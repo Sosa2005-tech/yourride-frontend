1f2752d366fd9ed9298a2ed202c2807aa122bb8d
Sosangkar saikia <saikiasosangkar11@gmail.com>
2025-08-21 01:40:24 +0530

    refactor: Move fleet logic to correct page structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Welcome to YourRide Guwahati!
-      </h1>
-    </main>
-  );
-}
